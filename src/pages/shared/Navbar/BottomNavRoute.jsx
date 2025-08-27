ea9d54e70ea200f30cfde15d044cc7c5c7f677ef
isalim079 <salimreza1025@gmail.com>
2025-08-27 11:41:05 +0600

    Bottom nav lines and design created perfectly

--- src/pages/shared/Navbar/BottomNavRoute.jsx
+++ src/pages/shared/Navbar/BottomNavRoute.jsx
@@ -5,16 +5,27 @@ import { ChevronDown } from "lucide-react";
 const BottomNavRoute = () => {
   return (
     <div>
-      <div className="max-w-[980px] mx-auto flex flex-row justify-between items-center font-sans text-sm text-gray-700 mt-5">
+      <div className=" flex flex-row justify-center items-center font-sans gap-5 text-sm text-gray-700 mt-4">
         {navLinkData.map((item) => (
           <Link
-            className="flex items-center gap-1"
+            className={`flex items-center gap-1`}
             to={item.link}
             key={item.id}
           >
-            {item.nav} <ChevronDown className="w-[14px] mt-[2px]" />
+            {item.nav} <ChevronDown className="w-[14px] mt-[2px]" />{" "}
+            {item?.id === 7 && (
+              <span className="flex justify-end ml-3">
+                <span className="h-[17px] w-[2px] bg-gray-300"></span>
+              </span>
+            )}
           </Link>
         ))}
+      </div>
+
+      {/* bottom lines */}
+      <div className="mt-3">
+        <div className="h-[1px] bg-black"></div>
+        <div className="h-[1px] bg-black mt-[2.5px]"></div>
       </div>
     </div>
   );
