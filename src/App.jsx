4cdf15d5ef8b5b5b935feb06a76c2a59cf06183d
isalim079 <salimreza1025@gmail.com>
2025-08-29 10:02:57 +0600

    Footer section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router";
 import "./App.css";
 import Navbar from "./pages/shared/Navbar/Navbar";
+import Footer from "./pages/shared/Footer/Footer";
 
 function App() {
   return (
     <div className="font-cheltenham">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/shared/Footer/FooterData.jsx
@@ -0,0 +1,379 @@
+export const footerLinks = {
+  news: [
+    {
+      id: 1,
+      name: "Home Page",
+      url: "#",
+    },
+    {
+      id: 2,
+      name: "U.S.",
+      url: "#",
+    },
+    {
+      id: 3,
+      name: "World",
+      url: "#",
+    },
+    {
+      id: 4,
+      name: "Politics",
+      url: "#",
+    },
+    {
+      id: 5,
+      name: "New York",
+      url: "#",
+    },
+    {
+      id: 6,
+      name: "Education",
+      url: "#",
+    },
+    {
+      id: 7,
+      name: "Sports",
+      url: "#",
+    },
+    {
+      id: 8,
+      name: "Business",
+      url: "#",
+    },
+    {
+      id: 9,
+      name: "Tech",
+      url: "#",
+    },
+    {
+      id: 10,
+      name: "Science",
+      url: "#",
+    },
+    {
+      id: 11,
+      name: "Weather",
+      url: "#",
+    },
+    {
+      id: 12,
+      name: "The Great Read",
+      url: "#",
+    },
+    {
+      id: 13,
+      name: "Obituaries",
+      url: "#",
+    },
+    {
+      id: 14,
+      name: "Headway",
+      url: "#",
+    },
+    {
+      id: 15,
+      name: "Visual Investigations",
+      url: "#",
+    },
+    {
+      id: 16,
+      name: "The Magazine",
+      url: "#",
+    },
+  ],
+  arts: [
+    {
+      id: 1,
+      name: "Book Review",
+      url: "#",
+    },
+    {
+      id: 2,
+      name: "Best Sellers Book List",
+      url: "#",
+    },
+    {
+      id: 3,
+      name: "Dance",
+      url: "#",
+    },
+    {
+      id: 4,
+      name: "Movies",
+      url: "#",
+    },
+    {
+      id: 5,
+      name: "Music",
+      url: "#",
+    },
+    {
+      id: 6,
+      name: "Pop Culture",
+      url: "#",
+    },
+    {
+      id: 7,
+      name: "Television",
+      url: "#",
+    },
+    {
+      id: 8,
+      name: "Theater",
+      url: "#",
+    },
+    {
+      id: 9,
+      name: "Visual Arts",
+      url: "#",
+    },
+  ],
+  lifestyle: [
+    {
+      id: 1,
+      name: "Health",
+      url: "#",
+    },
+    {
+      id: 2,
+      name: "Well",
+      url: "#",
+    },
+    {
+      id: 3,
+      name: "Food",
+      url: "#",
+    },
+    {
+      id: 4,
+      name: "Restaurant Reviews",
+      url: "#",
+    },
+    {
+      id: 5,
+      name: "Love",
+      url: "#",
+    },
+    {
+      id: 6,
+      name: "Travel",
+      url: "#",
+    },
+    {
+      id: 7,
+      name: "Style",
+      url: "#",
+    },
+    {
+      id: 8,
+      name: "Fashion",
+      url: "#",
+    },
+    {
+      id: 9,
+      name: "Real Estate",
+      url: "#",
+    },
+    {
+      id: 10,
+      name: "T Magazine",
+      url: "#",
+    },
+  ],
+  opinion: [
+    {
+      id: 1,
+      name: "Today's Opinion",
+      url: "#",
+    },
+    {
+      id: 2,
+      name: "Columnists",
+      url: "#",
+    },
+    {
+      id: 3,
+      name: "Editorials",
+      url: "#",
+    },
+    {
+      id: 4,
+      name: "Guest Essays",
+      url: "#",
+    },
+    {
+      id: 5,
+      name: "Op-Docs",
+      url: "#",
+    },
+    {
+      id: 6,
+      name: "Letters",
+      url: "#",
+    },
+    {
+      id: 7,
+      name: "Sunday Opinion",
+      url: "#",
+    },
+    {
+      id: 8,
+      name: "Opinion Video",
+      url: "#",
+    },
+    {
+      id: 9,
+      name: "Opinion Audio",
+      url: "#",
+    },
+  ],
+  more: [
+    {
+      id: 1,
+      name: "Audio",
+      url: "#",
+    },
+    {
+      id: 2,
+      name: "Games",
+      url: "#",
+    },
+    {
+      id: 3,
+      name: "Cooking",
+      url: "#",
+    },
+    {
+      id: 4,
+      name: "Wirecutter",
+      url: "#",
+    },
+    {
+      id: 5,
+      name: "The Athletic",
+      url: "#",
+    },
+    {
+      id: 6,
+      name: "Jobs",
+      url: "#",
+    },
+    {
+      id: 7,
+      name: "Video",
+      url: "#",
+    },
+    {
+      id: 8,
+      name: "Graphics",
+      url: "#",
+    },
+    {
+      id: 9,
+      name: "Trending",
+      url: "#",
+    },
+    {
+      id: 10,
+      name: "Live Events",
+      url: "#",
+    },
+    {
+      id: 11,
+      name: "Corrections",
+      url: "#",
+    },
+    {
+      id: 12,
+      name: "Reader Center",
+      url: "#",
+    },
+    {
+      id: 13,
+      name: "TimesMachine",
+      url: "#",
+    },
+    {
+      id: 14,
+      name: "The Learning Network",
+      url: "#",
+    },
+    {
+      id: 15,
+      name: "School of The NYT",
+      url: "#",
+    },
+    {
+      id: 16,
+      name: "inEducation",
+      url: "#",
+    },
+  ],
+  bottom: [
+    {
+      id: 1,
+      name: "NYTCs",
+      url: "#",
+    },
+    {
+      id: 2,
+      name: "Contact Us",
+      url: "#",
+    },
+    {
+      id: 3,
+      name: "Accessibility",
+      url: "#",
+    },
+    {
+      id: 4,
+      name: "Work with us",
+      url: "#",
+    },
+    {
+      id: 5,
+      name: "Advertise",
+      url: "#",
+    },
+    {
+      id: 6,
+      name: "T Brand Studio",
+      url: "#",
+    },
+    {
+      id: 7,
+      name: "Your Ad Choices",
+      url: "#",
+    },
+    {
+      id: 8,
+      name: "Privacy Policy",
+      url: "#",
+    },
+    {
+      id: 9,
+      name: "Terms of Service",
+      url: "#",
+    },
+    {
+      id: 10,
+      name: "Terms of Sale",
+      url: "#",
+    },
+    {
+      id: 11,
+      name: "Site Map",
+      url: "#",
+    },
+    {
+      id: 12,
+      name: "Help",
+      url: "#",
+    },
+    {
+      id: 13,
+      name: "Subscriptions",
+      url: "#",
+    },
+  ],
+};
