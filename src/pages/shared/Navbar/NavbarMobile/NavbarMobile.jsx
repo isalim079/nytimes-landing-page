41d5bc3c520e110d3c8e59b1deb6de5ee482d351
isalim079 <salimreza1025@gmail.com>
2025-09-02 13:06:17 +0600

    Navbar mobile responsive done

--- src/pages/shared/Navbar/NavbarMobile/NavbarMobile.jsx
+++ src/pages/shared/Navbar/NavbarMobile/NavbarMobile.jsx
@@ -34,10 +34,14 @@ const NavbarMobile = () => {
       <div
         className={`fixed top-0 left-0 h-full overflow-y-auto bg-white z-50 shadow-lg transition-transform duration-300 w-full ${drawerOpen ? "translate-x-0" : "-translate-x-full"} font-sans`}
       >
-        <div className="flex items-center justify-between px-4 py-4 border-b border-gray-200">
+        <div className="flex flex-col px-4 py-4">
           <button onClick={() => setDrawerOpen(false)}>
             <X className="w-6 h-6" />
           </button>
+          <div className="mt-5 flex items-center gap-3">
+            <input className="border border-gray-500 px-3 py-1 w-full" placeholder="SEARCH" type="search" name="navSearch" id="navSearch" />
+            <button className="bg-[#326891] text-white px-3 py-[5px]">Go</button>
+          </div>
         </div>
         <div className="px-4 py-2">
           {navLinkData.map((nav) => (
