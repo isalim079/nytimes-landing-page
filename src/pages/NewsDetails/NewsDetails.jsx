01984f40715b305fbd0ff0c586ea26ff8bb9e65d
isalim079 <salimreza1025@gmail.com>
2025-09-02 19:19:15 +0600

    News details mobile navbar added

--- src/pages/NewsDetails/NewsDetails.jsx
+++ src/pages/NewsDetails/NewsDetails.jsx
@@ -1,3 +1,4 @@
+import NavbarMobile from "../shared/Navbar/NavbarMobile/NavbarMobile";
 import NewsDetailsNav from "../shared/NewsDetailsNav/NewsDetailsNav";
 import Phase1 from "./Phase1/Phase1";
 import Phase2 from "./Phase2/Phase2";
@@ -6,6 +7,7 @@ const NewsDetails = () => {
   return (
     <div>
       <NewsDetailsNav />
+      <NavbarMobile />
       <div className="mb-5 px-4 lg:px-0">
         <Phase1 />
         <Phase2 />
